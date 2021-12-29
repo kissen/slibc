776f475859033de8e181ac37eb653fda61e42d45
Andres Schärtl <andreas@schaertl.me>
2021-12-29 16:19:18 +0100

    tests: add test that parsed number from argv

---
+++ tests/atoifromargv.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1) {
+        fputs("error: missing single argument\n", stderr);
+        return -1;
+    }
+
+    const int parsed = atoi(argv[1]);
+    return parsed;
+}
