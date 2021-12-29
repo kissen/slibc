bd84e9db51612534f93c2adfa3bac667cbce70ec
Andres Schärtl <andreas@schaertl.me>
2021-12-29 16:19:18 +0100

    slibc: add syscall1 function

--- include/slibc.h
+++ include/slibc.h
@@ -8,3 +8,14 @@ typedef signed char slibc_i8;
 
 typedef unsigned short slibc_u16;
 typedef signed short slibc_i16;
+
+typedef unsigned int slibc_u32;
+typedef signed int slibc_i32;
+
+typedef unsigned long int slibc_u64;
+typedef signed long int slibc_i64;
+
+/**
+ * Issue system call num with one argument.
+ */
+slibc_i64 syscall1(slibc_i64 num, slibc_i64 arg0);
