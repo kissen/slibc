4e8f288b4c399add9585e8a29e805d97b81003dd
Andres Schärtl <andreas@schaertl.me>
2021-12-29 16:19:18 +0100

    unistd: fix return type for syscalls

--- src/unistd/read.c
+++ src/unistd/read.c
@@ -4,7 +4,7 @@
 
 ssize_t read(int fd, void *buf, size_t count)
 {
-    const ssize_t result = slibc_syscall3(2, fd, (slibc_u64) buf, count);
+    const slibc_i64 result = slibc_syscall3(2, fd, (slibc_u64) buf, count);
 
     if (result < 0) {
         errno = labs(result);
--- src/unistd/write.c
+++ src/unistd/write.c
@@ -5,7 +5,7 @@
 
 ssize_t write(int fd, const void *buf, size_t count)
 {
-    const ssize_t result = slibc_syscall3(1, fd, (slibc_u64) buf, count);
+    const slibc_i64 result = slibc_syscall3(1, fd, (slibc_u64) buf, count);
 
     if (result < 0) {
         errno = labs(result);
