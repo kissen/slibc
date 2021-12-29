d09c5bd0ae44d508a064be377df339eaecff2e34
Andres Schärtl <andreas@schaertl.me>
2021-12-29 16:19:18 +0100

    include: add stdarg.h
    We just use the compiler builtins. I am actually not sure if we can even
    implement vargs w/o help from the compiler.

---
+++ include/stdarg.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define va_start(ap, last) __builtin_va_start(ap, last)
+#define va_arg(ap, type) __builtin_va_arg(ap, type)
+#define va_end(ap) __builtin_va_end(ap)
+#define va_copy(dest, src) __builtin_va_copy(dest, src)
