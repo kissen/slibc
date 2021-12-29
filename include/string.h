887d0c88ae22f2a7753d43b885dbe71906cca256
Andres Schärtl <andreas@schaertl.me>
2021-12-29 16:19:18 +0100

    string: add memmove function

--- include/string.h
+++ include/string.h
@@ -6,4 +6,13 @@
 
 // (7.24.2) Copying Functions
 
+/**
+ * Copy n bytes from src to dst. The first n bytes of src and dst may not
+ * overlap.
+ */
 void *memcpy(void *dst, const void *src, size_t n);
+
+/**
+ * Copy n bytes from src to dst. The first n byte of src and dst may overlap.
+ */
+void *memmove(void *dst, const void *src, size_t n);
