273fbc870e0fe86e68c4008d0fb9c96e7e073e4c
Andres Schärtl <andreas@schaertl.me>
2021-12-29 16:19:18 +0100

    stdlib: implement abort w/ kill

--- src/stdlib/abort.c
+++ src/stdlib/abort.c
@@ -1,7 +1,18 @@
+#include "signal.h"
+#include "stdio.h"
 #include "stdlib.h"
+#include "unistd.h"
 
 void abort(void)
 {
-    // TODO: implement w/ signals
+    // TODO: unblock signal first
+
+    const pid_t self = getpid();
+    const int sigabrt = 6;
+
+    if (kill(self, sigabrt) == -1) {
+        perror("abort: kill");
+    }
+
     exit(99);
 }
