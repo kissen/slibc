ea5e7a12d5b420899b3016e16b13309d2d909a4b
Andreas Schärtl <andreas@schaertl.me>
2022-06-27 17:55:59 +0200

    setjmp: set up stubs

--- include/setjmp.h
+++ include/setjmp.h
@@ -1,1 +1,19 @@
 #pragma once
+
+typedef struct
+{
+    unsigned long long ip;
+    unsigned long long sp;
+} jmp_buf;
+
+/**
+ * Write jump information to env.
+ *
+ * @return 0 on inital store and some non-zero value if being jumped to.
+ */
+int setjmp(jmp_buf env);
+
+/**
+ * Jump to env previously set up with a call to setjmp(3).
+ */
+void longjmp(jmp_buf env, int val);
