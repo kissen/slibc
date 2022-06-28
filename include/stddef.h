78b0bc3fcecc24a99d413e2bddaedd2428168da1
Andreas Schärtl <andreas@schaertl.me>
2022-06-28 16:30:10 +0200

    stddef: fix define for size_t

--- include/stddef.h
+++ include/stddef.h
@@ -3,7 +3,7 @@
 // (7.19) Common Definitions
 
 typedef long long int ptrdiff_t;
-typedef long long unsigned int size_t;
+typedef unsigned long int size_t;
 typedef long long int max_align_t;
 
 #define NULL ((void *)0)
