0a952baaffa14364337cbb85ebfa3399b2e50aa6
Andreas Schärtl <andreas@schaertl.me>
2022-06-29 19:02:20 +0200

    tests: add simple atoi tests

--- tests/atoi.c
+++ tests/atoi.c
@@ -1,6 +1,12 @@
-#include "munit.h"
+#include <munit.h>
+#include <stdlib.h>
 
 int main(void)
 {
+	munit_assert_int(atoi("0"), ==, 0);
+	munit_assert_int(atoi("1"), ==, 1);
+	munit_assert_int(atoi("10"), ==, 10);
+	munit_assert_int(atoi("100"), ==, 100);
+
 	return 0;
 }
